88d1261b29c86df494268f6c4478cbcb4f9b3bd3
JoBelmar <jose@iot4life>
2022-07-05 02:10:20 -0400

    Lista de propiedades

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,5 +1,12 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ROUTER_CONFIGURATION } from '@angular/router';
 import * as mapboxgl from 'mapbox-gl';
+
+interface MarcadorColor {
+  color: string;
+  marker?: mapboxgl.Marker;
+  centro?: [number, number]
+}
 
 @Component({
   selector: 'app-marcadores',
@@ -10,6 +17,18 @@ import * as mapboxgl from 'mapbox-gl';
         width: 100%;
         height: 100%;
       }
+      
+      .list-group {
+        position: fixed;
+        top: 20px;
+        right: 20px;
+        z-index: 999;        
+      }
+
+      li {
+        cursor: pointer;
+      }
+
     `
   ]
 })
@@ -24,6 +43,8 @@ export class MarcadoresComponent implements AfterViewInit {
 
   constructor() { }
 
+  marcadores: MarcadorColor[] = [];
+
   ngAfterViewInit(): void {
     // moviemiento del mapa
     this.mapa = new mapboxgl.Map({
@@ -33,14 +54,96 @@ export class MarcadoresComponent implements AfterViewInit {
       zoom: this.zoomLevel
     });
 
-    const markerHtml: HTMLElement = document.createElement('div');
+    this.leerLocalStorage();
+
+    /* const markerHtml: HTMLElement = document.createElement('div');
     markerHtml.innerHTML = 'Hola Mundo';
 
-    new mapboxgl.Marker({
+    const nuevoMarcador = new mapboxgl.Marker({
       element: markerHtml
     })
       .setLngLat(this.center)
-      .addTo(this.mapa);
+      .addTo(this.mapa); */
+
+    
   }
 
+  
+  agregarMarcador(){
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const nuevoMarcador = new mapboxgl.Marker({
+      draggable: true,
+      color: color
+    })
+    .setLngLat( this.center )
+    .addTo( this.mapa );
+    
+    this.marcadores.push({
+      color: color,
+      marker: nuevoMarcador
+    });  
+
+    this.guardarMarcadorLocalStorage();
+
+    nuevoMarcador.on('dragend', () => {
+      this.guardarMarcadorLocalStorage();
+    });
+  }
+
+  irMarcador(marcador: mapboxgl.Marker){    
+    this.mapa.flyTo({
+      center: marcador.getLngLat()
+    })
+  }
+
+  guardarMarcadorLocalStorage(){
+
+    const lngLatArr: MarcadorColor[] = [];
+    this.marcadores.forEach(m => {
+      const color = m.color;
+      const {lng, lat} = m.marker!.getLngLat();
+
+      lngLatArr.push({
+        color: color,
+        centro: [lng, lat]
+      })
+    });
+
+    localStorage.setItem('marcadores', JSON.stringify(lngLatArr));
+  }
+
+  leerLocalStorage(){
+    if(!localStorage.getItem('marcadores')){
+      return;
+    }
+
+    const lngLatArr: MarcadorColor[] = JSON.parse(localStorage.getItem('marcadores')!);
+    lngLatArr.forEach(m => {
+
+      const newMarker = new mapboxgl.Marker({
+        color: m.color,
+        draggable: true
+      })
+      .setLngLat(m.centro!)
+      .addTo(this.mapa)
+
+      this.marcadores.push({
+        marker: newMarker,
+        color: m.color
+      });
+
+      // escucha cuando se presiona sobre el objeto, se arrastra 
+      // y se deja de mover el mouse 
+      newMarker.on('dragend', () => {
+        this.guardarMarcadorLocalStorage();
+      });
+
+    });
+  }
+
+  borrarMarcador(i: number){
+    this.marcadores[i].marker!.remove();
+    this.marcadores.splice(i, 1);
+    this.guardarMarcadorLocalStorage();
+  }
 }
